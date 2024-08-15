af6d8495353c50173e072bd3f1df95cc785d807b
tranduy1dol <tmd.id2004@gmail.com>
2024-08-15 08:57:02 +0000

    refactor code

--- crates/settlement-clients/aptos/src/config.rs
+++ crates/settlement-clients/aptos/src/config.rs
@@ -1,11 +1,11 @@
 use dotenvy::dotenv;
+
 use settlement_client_interface::SettlementConfig;
 use utils::env_utils::get_env_var_or_panic;
 
 pub struct AptosSettlementConfig {
     pub node_url: String,
     pub private_key: String,
-    pub account_address: String,
     pub module_address: String,
     pub chain_id: String,
 }
@@ -15,9 +15,8 @@ impl SettlementConfig for AptosSettlementConfig {
         dotenv().expect("Failed to load .env file");
         let node_url = get_env_var_or_panic("APTOS_NODE_URL");
         let private_key = get_env_var_or_panic("APTOS_PRIVATE_KEY");
-        let account_address = get_env_var_or_panic("APTOS_ACCOUNT_ADDRESS");
         let module_address = get_env_var_or_panic("APTOS_MODULE_ADDRESS");
         let chain_id = get_env_var_or_panic("CHAIN_ID");
-        AptosSettlementConfig { chain_id, node_url, private_key, account_address, module_address }
+        AptosSettlementConfig { chain_id, node_url, private_key, module_address }
     }
 }
